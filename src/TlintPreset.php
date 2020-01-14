eac10d151551d1911de1a7905bc980a5be5a8722
Tom Witkowski <dev.gummibeer@gmail.com>
2020-01-14 12:54:57 +0100

    add tlint

---
+++ src/TlintPreset.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Elbgoods\CiTestTools;
+
+use Tighten\Linters\NoInlineVarDocs;
+use Tighten\Linters\NoParensEmptyInstantiations;
+use Tighten\Linters\RestControllersMethodOrder;
+use Tighten\Presets\LaravelPreset;
+
+class TlintPreset extends LaravelPreset
+{
+    public function getLinters() : array
+    {
+        return array_filter(parent::getLinters(), function(string $linter) {
+            return !in_array($linter, [
+                NoInlineVarDocs::class,
+                NoParensEmptyInstantiations::class,
+                RestControllersMethodOrder::class,
+            ]);
+        });
+    }
+}
