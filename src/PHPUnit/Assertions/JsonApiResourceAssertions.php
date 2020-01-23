cd6e5d6f5d73c429a2fbc8ae46d3dd3f6d09ffea
Tom Witkowski <dev.gummibeer@gmail.com>
2020-01-23 11:25:53 +0100

    add unified assertion for json api resource types

--- src/PHPUnit/Assertions/JsonApiResourceAssertions.php
+++ src/PHPUnit/Assertions/JsonApiResourceAssertions.php
@@ -2,10 +2,16 @@
 
 namespace Elbgoods\CiTestTools\PHPUnit\Assertions;
 
+use Closure;
 use Illuminate\Foundation\Testing\Assert as PHPUnit;
+use InvalidArgumentException;
+use OutOfBoundsException;
 
 trait JsonApiResourceAssertions
 {
+    /** @var array */
+    protected static $jsonApiResourceAssertions = [];
+
     public static function assertIsJsonApiResource(array $actual, ?string $type = null, ?int $id = null): void
     {
         PHPUnit::assertArrayHasKey('id', $actual);
@@ -20,5 +26,25 @@ trait JsonApiResourceAssertions
         if ($type !== null) {
             PHPUnit::assertEquals($type, $actual['type']);
         }
+    }
+
+    public static function assertIsJsonApiResourceOfType(string $type, $actual, ...$params): void
+    {
+        static::assertIsJsonApiResource($actual);
+
+        if (! array_key_exists($type, static::$jsonApiResourceAssertions)) {
+            throw new OutOfBoundsException(sprintf('There is no assertion registered for type "%s".', $type));
+        }
+
+        call_user_func(static::$jsonApiResourceAssertions[$type], $actual, ...$params);
+    }
+
+    public static function registerJsonApiResourceAssertion(string $type, Closure $assertion): void
+    {
+        if (array_key_exists($type, static::$jsonApiResourceAssertions)) {
+            throw new InvalidArgumentException(sprintf('There is already an assertion registered for type "%s".', $type));
+        }
+
+        static::$jsonApiResourceAssertions[$type] = $assertion;
     }
 }
