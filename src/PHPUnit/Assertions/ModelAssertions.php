af4bc2e8106b9c02f7ea692ad63b616c20be1b68
Tom Witkowski <dev.gummibeer@gmail.com>
2020-11-05 12:11:17 +0100

    replace #39

--- src/PHPUnit/Assertions/ModelAssertions.php
+++ src/PHPUnit/Assertions/ModelAssertions.php
@@ -3,10 +3,37 @@
 namespace Elbgoods\CiTestTools\PHPUnit\Assertions;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Testing\Constraints\HasInDatabase;
 use PHPUnit\Framework\Assert as PHPUnit;
 
 trait ModelAssertions
 {
+    /**
+     * @param Model|string  $table
+     * @param array $data
+     * @param string|null $connection
+     *
+     * @return void
+     */
+    public static function assertExists($table, array $data = [], ?string $connection = null): void
+    {
+        if ($table instanceof Model) {
+            $model = $table;
+
+            $table = $model->getTable();
+            $connection = $model->getConnectionName();
+            $data = [
+                $model->getKeyName() => $model->getKey(),
+            ];
+        }
+
+        PHPUnit::assertThat(
+            $table,
+            new HasInDatabase(DB::connection($connection), $data)
+        );
+    }
+
     /**
      * @param Model $expected
      * @param Model|mixed $actual
